7c7d52b140b94e5e1f758afd585385f8c429fa74
Rodolfo Leal <rodolfo.pk@gmail.com>
2019-02-09 08:06:56 -0200

    Odd or even project

---
+++ EvenOrOdd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	slices := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, number := range slices {
+		if number%2 == 0 {
+			fmt.Println(number, " is Even")
+		} else {
+			fmt.Println(number, " is ODD")
+		}
+	}
+}
