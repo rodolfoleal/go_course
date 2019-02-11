39524842f26a8091ecce8a246470ce4aee2237ca
rodolfo.vieira <rodolfo.vieira@castgroup.com.br>
2019-02-11 11:30:55 -0200

    First maps examples

--- map/main.go
+++ map/main.go
@@ -3,20 +3,27 @@ package main
 import "fmt"
 
 func main() {
-
+	// Declaring a map #1
 	// var colors map[string]string
+	// Declaring a map #2
+	// colors := make(map[string]string)
 
-	colors := make(map[string]string)
-
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// 	"blue":  "#0000ff",
-	// }
+	// Declaring a map #3
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
 
 	colors["white"] = "#ffffff"
 
 	delete(colors, "white")
 
 	fmt.Print(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex for : ", color, " is :", hex)
+	}
 }
