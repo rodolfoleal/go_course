c8b2ee3671ad2a02ac8dd8bd4ca2dfb0c758664b
rodolfo.vieira <rodolfo.vieira@castgroup.com.br>
2019-02-11 11:26:57 -0200

    First maps examples

---
+++ map/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// 	"blue":  "#0000ff",
+	// }
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
+
+	fmt.Print(colors)
+}
