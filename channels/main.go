c1f6c820585372aca2c0e41f91074bc0b232c518
Rodolfo Leal <rodolfo.pk@gmail.com>
2019-02-18 22:51:19 -0300

    Status Check program.

---
+++ channels/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://bomnegocio.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, error := http.Get(link)
+	if error != nil {
+		fmt.Println(link, " Must be down!")
+		return
+	}
+
+	fmt.Println(link, " is up an running!")
+}
